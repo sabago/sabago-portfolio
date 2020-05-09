e4d643c68006da4788d4f6587c2df4f17a519166
sabago <sandra.abago@gmail.com>
2020-05-09 19:14:28 -0400

    Updates

---
+++ precache-manifest.6625d6d9e8dcd7660a70ff75f33b70f0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8776b1b87f7bb81b7074e26b8461df7a",
+    "url": "/sabago-portfolio/index.html"
+  },
+  {
+    "revision": "bd50cc75f278e541ef89",
+    "url": "/sabago-portfolio/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "65202f1671b0cf8bde25",
+    "url": "/sabago-portfolio/static/css/main.ce8d5892.chunk.css"
+  },
+  {
+    "revision": "bd50cc75f278e541ef89",
+    "url": "/sabago-portfolio/static/js/2.4b74ec65.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/sabago-portfolio/static/js/2.4b74ec65.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "65202f1671b0cf8bde25",
+    "url": "/sabago-portfolio/static/js/main.f40515ca.chunk.js"
+  },
+  {
+    "revision": "443be611b2809871ea19",
+    "url": "/sabago-portfolio/static/js/runtime-main.1b3e15f3.js"
+  },
+  {
+    "revision": "05f905ef7bb37fa9456fc6acb7b18eb7",
+    "url": "/sabago-portfolio/static/media/blacksand.05f905ef.jpg"
+  }
+]);
